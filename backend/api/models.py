640220eb012396c40d07432a66de3df4d6d91d9c
Daze <ivanrakov540@gmail.com>
2023-11-21 15:29:57 +0500

    add authorization backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    phone_number = models.CharField(max_length=12)
+    role = models.CharField(max_length=25)
+    first_work_day = models.DateTimeField(blank=True, null=True)
+    last_work_day = models.DateTimeField(blank=True, null=True)
+    salary = models.PositiveIntegerField(default=0)
