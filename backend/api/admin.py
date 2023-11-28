7fa67bd4a0aed82c6afdc81d1374fc575c458377
tripleUnderscoreL <triple.krg@gmail.com>
2023-11-28 19:42:08 +0500

    add models to admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Employee
+from .models import *
 
 
 admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Delivery)
+admin.site.register(Product)
+admin.site.register(ItemsOrdered)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 import uuid
+
 
 class Employee(AbstractUser):
     phone_number = models.CharField(max_length=12)
